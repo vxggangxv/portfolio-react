f0e998050feccb1d86966ca225d92b3242195b40
vxggangxv <vxggangxv@naver.com>
2020-04-06 18:23:25 +0900

    edit search

---
+++ src/store/project.js
@@ -0,0 +1,69 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const projectListData = [
+  {
+    name: 'Conacoin1',
+    date: '2020.03.25',
+    stack: 'html5, css3, javascript'
+  },
+  {
+    name: 'Conacoin2',
+    date: '2020.03.25',
+    stack: 'html5, css3, javascript, react'
+  },
+  {
+    name: 'Conacoin3',
+    date: '2020.03.25',
+    stack: 'html5, css3, javascript, express'
+  },
+];
+
+export const slice = createSlice({
+  name: 'project',
+  initialState: {
+    projectList: projectListData,
+    searchList: projectListData
+  },
+  reducers: {
+    increment: state => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1;
+    },
+    decrement: state => {
+      state.value -= 1;
+    },
+    searchByStack: (state, action) => {
+      let word = action.payload.toLowerCase();
+      var splitWordList = word.split(' ');
+      let searchList = [];
+      splitWordList.forEach((word) => {
+        // console.log(word);
+        // console.log(i);
+        state.projectList.forEach((item, i) => {
+          if (item.stack.indexOf(word) > -1) {
+            // console.log(item);
+            searchList.push(item);
+          }
+        });
+      })
+      console.log(searchList);
+      const set = Array.from(new Set(searchList));
+      console.log(set);
+      state.searchList = set;
+
+      // state.searchList = state.projectList.filter(str => {
+      //   return item.stack.indexOf(str) !== -1;
+      // })
+    },
+  },
+});
+
+export const { searchByStack } = slice.actions;
+
+export const selectProjectList = state => state.project.projectList;
+export const selectSearchList = state => state.project.searchList;
+
+export default slice.reducer;
